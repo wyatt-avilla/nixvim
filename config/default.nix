3f516d7703189cb0b723134bdda5de538128fd7e
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-21 19:50:42 -0700

    telescope

--- config/default.nix
+++ config/default.nix
@@ -4,11 +4,13 @@
     ./options.nix
     ./keymaps.nix
     ./bufferline.nix
+    ./telescope.nix
   ];
 
   plugins = {
     lualine.enable = true;
     gitsigns.enable = true;
     wakatime.enable = true;
+    yazi.enable = true;
   };
 }
