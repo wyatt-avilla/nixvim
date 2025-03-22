1666419524db8620c67dc96a9e820bbd6086bc9d
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-21 19:12:11 -0700

    options

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,7 @@
 {
   # Import all your configuration modules here
   imports = [
+    ./options.nix
     ./keymaps.nix
     ./bufferline.nix
   ];
---
+++ config/options.nix
@@ -0,0 +1,21 @@
+{
+  globals.mapleader  = " ";
+
+  opts = {
+    nu = true;
+    relativenumber = true;
+
+    tabstop = 4;
+    softtabstop = 4;
+    shiftwidth = 4;
+    smartindent = true;
+
+    swapfile = false;
+    backup = false;
+
+    hlsearch = false;
+    incsearch = true;
+
+    scrolloff = 8;
+  };
+}
