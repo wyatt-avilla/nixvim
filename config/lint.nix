d7138eabc7013ec8dda82d97a27e8159cc7ad510
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-03-23 14:51:21 -0700

    very strict lints

--- config/lint.nix
+++ config/lint.nix
@@ -10,5 +10,13 @@
       cpp = [ "clangtidy" ];
       markdown = [ "markdownlint" ];
     };
+
+    luaConfig.post = ''
+      local lint = require("lint")
+
+      lint.linters.shellcheck.args = vim.list_extend({"--enable=all"}, lint.linters.shellcheck.args)
+      lint.linters.mypy.args = vim.list_extend({"--strict"}, lint.linters.mypy.args)
+      lint.linters.clangtidy.args = vim.list_extend({"-checks='*,-fuchsia-*,-google-*,-android-*,-llvm-header-guard'"}, lint.linters.clangtidy.args)
+    '';
   };
 }
