bf979bc7a831496e3139e8ca1145de3071388c8d
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-04-26 20:37:19 -0700

    highlight on yank

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -5,5 +5,9 @@
       event = [ "Filetype" ];
       pattern = [ "markdown,gitcommit,tex" ];
     }
+    {
+      event = [ "TextYankPost" ];
+      callback.__raw = "function() vim.highlight.on_yank() end";
+    }
   ];
 }
