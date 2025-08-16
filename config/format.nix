ef4ebc437bdbf6ecb0769c378d648f9d2f2c4a86
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-15 18:03:41 -0700

    formatting

--- config/format.nix
+++ config/format.nix
@@ -5,14 +5,13 @@
 
     settings = {
       format_on_save = ''
-                function(bufnr)
-                  -- Disable with a global or buffer-local variable
-                  if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                    return
-                  end
-                  return { timeout_ms = 1000, lsp_fallback = false }
-                end
-        	  '';
+        function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 1000, lsp_fallback = false }
+        end
+      '';
 
       formatters_by_ft = {
         python = [ "ruff_format" ];
@@ -46,7 +45,6 @@
       command.__raw = ''
         function(args)
            if args.bang then
-            -- FormatDisable! will disable formatting just for this buffer
             vim.b.disable_autoformat = true
           else
             vim.g.disable_autoformat = true
@@ -61,7 +59,6 @@
       command.__raw = ''
         function(args)
            if args.bang then
-            -- FormatEnable! will enable formatting just for this buffer
             vim.b.disable_autoformat = false
           else
             vim.g.disable_autoformat = false
