9d186d0cbec878ba4be318e2ace0a0d5f6c4a839
Wyatt Avilla <wyatt@linux.com>
2026-02-19 01:32:52 -0800

    neogit

--- config/git/default.nix
+++ config/git/default.nix
@@ -3,5 +3,8 @@
     ./fugitive.nix
     ./gitsigns.nix
     ./fzf.nix
+    ./neogit.nix
   ];
+
+  plugins.diffview.enable = true;
 }
--- config/git/neogit.nix
+++ config/git/neogit.nix
@@ -1,6 +1,44 @@
 {
   plugins.neogit = {
     enable = true;
-
+    settings = {
+      mappings = {
+        popup = {
+          i = false;
+        };
+        status = {
+          "=" = "Toggle";
+        };
+      };
+    };
   };
+
+  keymaps = [
+    {
+      key = "<leader>gg";
+      action = "<cmd>Neogit<CR>";
+      mode = "n";
+      options.desc = "Open Neogit";
+    }
+    {
+      key = "<leader>gp";
+      action = "<cmd>Neogit pull<CR>";
+      mode = "n";
+      options.desc = "Git pull";
+    }
+    {
+      key = "<leader>gP";
+      action = "<cmd>Neogit push<CR>";
+      mode = "n";
+      options.desc = "Git push";
+    }
+  ];
+
+  autoCmd = [
+    {
+      event = [ "FileType" ];
+      pattern = [ "NeogitStatus" ];
+      command = "lua vim.keymap.set('n', 'i', 'k', { buffer = true, silent = true })";
+    }
+  ];
 }
